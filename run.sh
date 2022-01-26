833d1385d73bae4bcc1ffb6c19c23079ce9a0fbf
kremenevskiy <kremeneuski@gmail.com>
2022-01-27 00:17:21 +0300

    test model script

--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ wget -nc http://cs231n.stanford.edu/tiny-imagenet-200.zip
 echo "Unzipping Tiny ImageNet..."
 unzip -qq tiny-imagenet-200.zip
 echo "Divide Validation folder to Validation / test (50/50) ..."
+mv tiny-imagenet-200/test tiny-imagenet-200/test_save
 python3 prepare_data.py
 mkdir models
 echo "Copying all images to ./tiny-224..."
 cp -r tiny-imagenet-200 tiny-224
 echo "Resizing images to 224 x 224..."
 python3 resize.py
+
 
